07a1b35fd06d97f06b14ecf372ee13098c1fc463
salman-farcy <salmanfarcy253@gmail.com>
2024-03-05 10:01:14 +0600

    add register route

--- server/server.js
+++ server/server.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
      res.status(200).send("Welcome to world best mern series by salman farcy");
 });
 
+app.get("/register", (req, res) => {
+     res.status(200).send("Welcome to registration page");
+});
+
 app.listen(PORT, () => {
      console.log(`server is running at port ${PORT}`);
-});
+}); 
