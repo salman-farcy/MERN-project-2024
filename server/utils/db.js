deca029f8633fe78520d8e2908b4c84eb8352819
salman-farcy <salmanfarcy253@gmail.com>
2024-03-15 12:58:52 +0600

    A database connection has been made to the server

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
+require('dotenv').config()
 
 
 
-const URI = "mongodb://127.0.0.1:27017/mern_admin";
-
+// const URI = "mongodb://127.0.0.1:27017/mern_admin";
+const uri = `mongodb+srv://${process.env.MONGODB_USER_NAME}:${process.env.MONGODB_USER_PASSWORD}@cluster0.rb5g6hh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 
 const connectDb = async () => {
     try {
-        await mongoose.connect(URI);
+        await mongoose.connect(uri);
         console.log("connnection successful to DB");
 
     }
