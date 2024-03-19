479818a8d02ef493d23e9e4c7aeb750de6e13b35
salman-farcy <salmanfarcy253@gmail.com>
2024-03-19 16:40:26 +0600

    (jwt) Notes

--- server/models/user-modle.js
+++ server/models/user-modle.js
@@ -46,6 +46,18 @@ userSchema.pre('save', async function (next) {
 });
 
 
+//TODO: What is JWT?
+//* JSON Web Tokens (JWT) is an open standard (RFC 7519) that defines a compact and self-contained way for securely transmitting information between parties as a JSON object.
+//* JWTs are often used for authentication and authorization in web applications.
+//?     Authentication: Verifying the identity of a user or client.
+//?     Authorization: Determining what actions a user or client is allowed to perform.
+
+//TODO: Components of a JWT:
+//? Header: Contains metadata about the token, such as the type of token and the signing algorithm being used.
+//? Payload: Contains claims or statements about an entity (typically, the user) and additional data. Common claims include user ID, username, and expiration time.
+//? Signature: To verify that the sender of the JWT is who it says it is and to ensure that the message wasn't changed along the way, a signature is included.
+
+
 //define the model or the collection name
-const User = new mongoose.model("USER", userSchema);
+const User = new mongoose.model("User", userSchema);
 module.exports = User;
